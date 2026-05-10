36c9de624b4e7a3bdaa4192c0f36c2e8c9482710
benedettafazzari-ux <benedettafazzari@gmail.com>
2026-05-10 18:10:48 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+export default function App() {
+  return (
+    <div
+      style={{
+        background: "#005bbb",
+        color: "white",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        fontFamily: "Arial"
+      }}
+    >
+      <h1>AVIS Calabria</h1>
+    </div>
+  );
+}
